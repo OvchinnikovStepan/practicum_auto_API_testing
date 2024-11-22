2bce03605590eac81d25d42df65aa31af28aec5d
Ovchinnikov_Stepan <ovstal@gmail.com>
2024-11-22 11:55:27 +0600

    Deleting example files

--- data/create_user_data.py
+++
@@ -1,7 +0,0 @@
-"""Дата файл для тестирования api reqres, create user"""
-from model.create_model import RequestCreateUserModel
-
-# данные для тестов ('request_parameters')
-data = (RequestCreateUserModel(name='test', job='bobr'),
-        RequestCreateUserModel(name='123', job=''),
-        RequestCreateUserModel(name='Бобр Добр', job='#@^&T'))
--- fixture/reqres_api.py
+++
@@ -1,8 +0,0 @@
-"""Фикстуры ReqRes API"""
-import pytest
-from api.reqres_api import ReqresApi
-
-@pytest.fixture(scope="function")
-def reqres_api() -> ReqresApi:
-    """Коннект к ReqRes API"""
-    return ReqresApi()
